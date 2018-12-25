91590aeebcfcb5ab5bcb9b9071556c5258388dc3
Larry Botha <larry@fixate.it>
2018-12-25 11:19:50 +0200

    add jest-watch-typeahead

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   runner: 'jest-runner-newman',
 
   testMatch: ['<rootDir>/tests/**/*.test.js'],
+
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
